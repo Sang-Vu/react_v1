6bdf6602deb31a64ed6c0075a5ed27febca06b9f
Sang-Vu <sangvunn@gmail.com>
2024-03-08 15:06:27 +0700

    Update react version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+//import ReactDOM from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+  ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
